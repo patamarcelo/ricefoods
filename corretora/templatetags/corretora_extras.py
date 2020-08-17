a654aec037daff4d97ee45c025050e7d3f0bae6f
Marcelo Pata <marcelopata@MacPhata.local>
2020-08-17 12:46:10 -0300

    mes ingles

--- corretora/templatetags/corretora_extras.py
+++ corretora/templatetags/corretora_extras.py
@@ -8,37 +8,35 @@ from datetime import datetime
 import locale
 import time
 
+
 @register.filter
 def nome_mes(datemont):
-    locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+
     numeromes = datetime.now().month
-    mes = calendar.month_name[numeromes] 
+    mes = calendar.month_name[numeromes]
     return mes
+
 
 @register.filter
 def nome_mes_anterior(datemont):
-    locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+
     numeromes = datetime.now().month
     if numeromes == 1:
         mesanterior == 12
-        return calendar.month_name[mesanterior] 
+        return calendar.month_name[mesanterior]
     else:
         mesanterior = numeromes - 1
         return calendar.month_name[mesanterior]
 
+
 @register.filter
 def nome_mes_anteanterior(datemont):
-    locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+
     numeromes = datetime.now().month
     if numeromes == 1:
         mesanterior == 11
-        return calendar.month_name[mesanterior] 
+        return calendar.month_name[mesanterior]
     else:
         mesanterior = numeromes - 2
         return calendar.month_name[mesanterior]
 
-
-        
-
-
-        
