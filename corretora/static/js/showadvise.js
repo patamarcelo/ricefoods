2cc5e792d56c8b648c4a43813752570d7f4fdd83
Marcelo Pata <56968177+patamarcelo@users.noreply.github.com>
2021-04-30 13:49:41 -0300

    mudancas css templates and copyandpaste nos outros templates

--- corretora/static/js/showadvise.js
+++ corretora/static/js/showadvise.js
@@ -2,13 +2,9 @@ $(document).ready(function () {
   $(".copy-data").click(function () {
     const t = 800;
     const t1 = 400;
-    const t2 = 500;
+    const t2 = 990;
 
-    // const attr = e.target.attr("data-clipboard-text");
-    // alert('marcelo'));
     const valueeeee = $(this).attr("data-clipboard-text");
-    // console.log(valueeeee);
-    // console.log(typeof valueeeee);
     var elementtextadv = document.getElementById("cargasmotcopy");
     elementtextadv.innerHTML = valueeeee;
     $("#clickeadvise").slideToggle(t1).delay(t2).slideToggle(t);
