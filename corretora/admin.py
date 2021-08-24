83bcece3fac3dd0395cb6463357aee0daac05883
Marcelo Pata <56968177+patamarcelo@users.noreply.github.com>
2021-08-24 14:40:59 -0300

    mostrar pedido aberto fechado admin

--- corretora/admin.py
+++ corretora/admin.py
@@ -336,7 +336,7 @@ class TransportadoraAdmin(admin.ModelAdmin):
 
 # @admin.register(Carga)
 class CargaAdmin(SimpleHistoryAdmin):
-    list_display = ('placa','motorista','situacao','pedido','get_data','buonny','tac','ordem','get_fornecedor','get_cidade_fornecedor','get_cliente','transp','veiculo','notafiscal','gera_comi_frete','tem_comprovante','comi_frete_ton','get_modificado',)
+    list_display = ('placa','motorista','situacao','pedido','get_pedido_aberto','get_data','buonny','tac','ordem','get_fornecedor','get_cidade_fornecedor','get_cliente','transp','veiculo','notafiscal','gera_comi_frete','tem_comprovante','comi_frete_ton','get_modificado',)
     fieldsets = (
     ('Agendamento', {
         'fields': ('pedido', ('data', 'buonny') )
@@ -365,6 +365,16 @@ class CargaAdmin(SimpleHistoryAdmin):
     search_fields = ['pedido__contrato','situacao','data','pedido__fornecedor__nome','placa','pedido__cliente__nome','pedido__tipo','motorista','peso','veiculo','buonny','notafiscal','notafiscal2','valornf']
     history_list_display = ["situacao","ordem","get_data","peso","agendamento","notafiscal","pedido","motorista","placa","obs","valornf","valor_mot","changed_fields"]
 
+    def get_pedido_aberto(self, obj):
+        if obj.pedido.situacao == 'a':
+            obj_res = True
+        else:
+            obj_res = False
+        return obj_res
+    
+    get_pedido_aberto.boolean = True
+    get_pedido_aberto.short_description = u"Ped. Sit."
+    
     def tem_comprovante(self, obj):
         if obj.comprovante_descarga:
             obj_res = True
